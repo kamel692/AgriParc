e1357881f0353e5abd3ccb6560bd5ea84d98774f
Nils <nils.vovan@gmail.com>
2019-10-22 14:20:45 +0200

    Add OpenStreetMap with library LeafLet

--- FrontEnd/src/app/pages/maps/maps.component.ts
+++ FrontEnd/src/app/pages/maps/maps.component.ts
@@ -1,33 +1,28 @@
-import { Component,OnInit } from '@angular/core';
-
-declare var google: any;
-declare var ol:any;
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
-    moduleId: module.id,
-    selector: 'maps-cmp',
-    templateUrl: 'maps.component.html'
+  moduleId: module.id,
+  selector: 'maps-cmp',
+  templateUrl: 'maps.component.html',
+  styleUrls: ['maps.component.css']
 })
 
 export class MapsComponent implements OnInit {
 
+  public myIcon = L.icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+  });
 
+  map: any;
+  ngOnInit() {
 
-  
-    map: any;
-    ngOnInit() {
-            
-        this.map = new ol.Map({
-            target: 'map',
-            layers: [
-              new ol.layer.Tile({
-                source: new ol.source.OSM()
-              })
-            ],
-            view: new ol.View({
-              center: ol.proj.fromLonLat([4.866659, 45.757043]),
-              zoom: 18
-            })
-          });
-    }
+    this.map = L.map('mapid').setView([45.757043, 4.866659], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org    /copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    L.marker([45.757043, 4.866659], {icon: this.myIcon}).bindPopup('Je suis un Frugal Marqueur').addTo(this.map).openPopup();
+  }
 }
